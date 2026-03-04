d71e6779a0225a0e02d3e7257ab48374eaa524ac
Thiago <tpkoepsel@gmail.com>
2026-03-04 19:46:51 -0300

    fix: ajeita a classe calculardoradepreco

--- Mês 02 - Fevereiro/25.02.2026/CalculadoraDePreco.java
+++ Mês 02 - Fevereiro/25.02.2026/CalculadoraDePreco.java
@@ -9,13 +9,20 @@
  */
 public class CalculadoraDePreco {
 
-	public static double calcular(double preco) {
+	public static double calcularImposto(double preco, double imposto) {
 		double resultado = preco;
-		resultado = resultado + (preco * 0.10);
+		resultado = resultado + (preco * imposto);
+		return resultado;
+	}
+
+	public static double calcularDesconto(double preco, double desconoto){
+		double resultado = preco;
+		resultado = resultado - (preco * desconoto);
 		return resultado;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(calcular(100));
+		System.out.println(calcularImposto(100, 0.25));
+		System.out.println(calcularDesconto(100, 0.10));
 	}
 }
